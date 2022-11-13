027de5df5a8130956b5f8c19df8f89e8bc01a47a
Vedant Gandhi <110923021+vedu443@users.noreply.github.com>
2022-11-13 11:42:23 +0530

    Update AIBrain.py

--- AIBrain.py
+++ AIBrain.py
@@ -1,5 +1,5 @@
 # Open AI
-fileopen = open("Data\\Api.txt","r")
+fileopen = open("Api.txt","r")
 API = fileopen.read()
 fileopen.close()
 
@@ -14,7 +14,7 @@ load_dotenv()
 completion = openai.Completion()
 
 def ReplyBrain(question,chat_log = None):
-    FileLog = open("DataBase\\chat_log.txt","r")
+    FileLog = open("chat_log.txt","r")
     chat_log_template = FileLog.read()
     FileLog.close()
 
@@ -25,7 +25,7 @@ def ReplyBrain(question,chat_log = None):
     response = completion.create(model = "text-davinci-002", prompt=prompt, temperature = 0.2, max_tokens = 60, top_p = 0.3, frequency_penalty = 0.5, presence_penalty = 0)    
     answer = response.choices[0].text.strip()
     chat_log_template_update = chat_log_template + f"\nYou : {question} \nJarvis : {answer}"   
-    FileLog = open("DataBase\\chat_log.txt","w")
+    FileLog = open("chat_log.txt","w")
     FileLog.write(chat_log_template_update)
     FileLog.close()
     return answer
